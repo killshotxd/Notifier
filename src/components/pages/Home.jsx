0f016066323659383cbcf728c269598f352b10fe
killshotxd <hassanansari211@gmail.com>
2023-05-01 19:36:18 +0530

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,12 @@
 import Inbox from "./Inbox";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "../layouts/Footer";
+
 const Home = () => {
   return (
     <>
       <ToastContainer />
       <Inbox />
-      <Footer />
     </>
   );
 };
